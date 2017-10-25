bacc6028eb0d6d78bfe270efcecb7d17480ba300
Tamzida Azad <1421064@iub.edu.bd>
2017-10-25 03:30:55 +0000

    Assignment 10

--- routes/article.server.routes.js
+++ routes/article.server.routes.js
@@ -3,15 +3,31 @@ module.exports = function(app){
  var articles = require('./../controllers/articles.server.controller.js');
  var users = require('./../controllers/users.server.controller.js');
 
- app.route('/api/articles')
+ app.route('/articles/new')
+	.get(articles.create);
+//	.post(users.requiresLogin, articles.create);
+
+  app.route('/articles/all')
+	.get(articles.all);
+ // .delete(users.requiresLogin, articles.delete);
+
+	app.route('/articles/:edit')
+	.get(articles.edit);
+	
+		app.route('/articles/:view')
+	.get(articles.view);
+//	.put(users.requiresLogin, articles.update);
+
+app.route('/api/articles/')
 	.get(articles.list)
 	.post(users.requiresLogin, articles.create);
+	
 
-  app.route('/api/articles/:articleId')
+app.route('/api/articles/edit/:articleId')
 	.get(articles.read)
-  .delete(users.requiresLogin, articles.delete);
-
-	app.route('/api/articles/edit/:articleId')
+	.put(users.requiresLogin, articles.delete);
+	
+app.route('/api/articles/edit/:articleId')
 	.get(articles.read)
 	.put(users.requiresLogin, articles.update);
 
